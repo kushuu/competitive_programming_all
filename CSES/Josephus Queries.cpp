12a4790123e191c4fb0484f057fc44873c250ff4
kushuu <sonikushu007@gmail.com>
2021-03-21 10:20:28 +0530

    organized files

---
+++ CSES/Josephus Queries.cpp
@@ -0,0 +1,38 @@
+/****************************************************************
+Author: kushuu   File: Josephus Queries.cpp    Date: Sat Mar 20 2021
+****************************************************************/
+
+#include <bits/stdc++.h>
+//shorts
+#define ll long long int
+#define sll stack<long long int>
+#define vll vector<long long int>
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define vpll vector<pair<long long int, long long int>>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define fo(i,x,y) for(long long i = x; i < y; i++)
+#define MOD 1000000007
+#define endl "\n"
+#define F first
+#define S second
+#define s(a) a.size()
+
+//program specific shorts (if any)
+//
+using namespace std;
+
+ll getlcm(ll a, ll b) {
+    return (a*b)/__gcd(a, b);
+}
+
+int main() {
+    fastIO;
+    ll t; cin >> t;
+    while(t--) {
+        ll n, k; cin >> n >> k;
+        cout << (k*2)%n << endl;
+    }
+    return 0;
+}
