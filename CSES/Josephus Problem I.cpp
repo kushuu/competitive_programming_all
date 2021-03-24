9aa9cc52fa0822168f1fcbbec2a83bafed1aa4c3
kushuu <sonikushu007@gmail.com>
2021-03-24 16:36:02 +0530

    failed attempt at josephus problem 1

---
+++ CSES/Josephus Problem I.cpp
@@ -0,0 +1,36 @@
+/****************************************************************
+Author: kushuu   File: Josephus Problem I.cpp    Date: Wed Mar 24 2021
+****************************************************************/
+
+#include <bits/stdc++.h>
+//shorts
+#define ll long long int
+#define sll stack<long long int>
+#define vll vector<long long int>
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define vpll vector<pair<long long int, long long int>>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define fo(i,x,y) for(long long i = x; i < y; i++)
+#define MOD 1000000007
+#define endl "\n"
+#define F first
+#define S second
+#define s(a) a.size()
+
+//program specific shorts (if any)
+//
+using namespace std;
+
+ll getlcm(ll a, ll b) {
+    return (a*b)/__gcd(a, b);
+}
+
+int main() {
+    fastIO;
+    ll n; cin >> n;
+    for(ll i = 2; i <= n; i += 2) cout << i << " " ;
+    for(ll i = 1; i <= n; i += 2) cout << i << " " ;
+    return 0;
+}
