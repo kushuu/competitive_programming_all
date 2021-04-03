302c98b4de09e44bba75eba1421d60ba0be0448d
kushuu <sonikushu007@gmail.com>
2021-04-04 00:25:24 +0530

    500 pointsss 🥳

--- CodeChef/KAVGMAT.cpp
+++ CodeChef/KAVGMAT.cpp
@@ -33,13 +33,38 @@ int main() {
     while(t--) {
         ll n, m, k, ans = 0;
         cin >> n >> m >> k;
-        ll mat[n][m];
+        vector<vector<ll>> mat(n , vector<ll> (m, 0));
         fo(i, 0, n) fo(j, 0, m) {
             cin >> mat[i][j];
             if(mat[i][j] >= k) ans++;
         }
-        fo(order, 1, min(n,m)) {
-            
+        if(!ans) {
+            cout << 0 << endl;
+            continue;
+        }
+
+        vector<vector<ll>> pref(n , vector<ll> (m, 0));
+        pref[0][0] = mat[0][0];
+        for (int i = 1; i < m; i++) pref[0][i] = pref[0][i - 1] + mat[0][i];
+        for (int i = 1; i < n; i++) pref[i][0] = pref[i - 1][0] + mat[i][0];
+    
+        for (int i = 1; i < n; i++) for (int j = 1; j < m; j++) pref[i][j] = pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1] + mat[i][j];
+        
+        // for(auto i : pref) {
+        //     for(auto j : i) cout << j << " " ; cout << endl;
+        // }
+
+        fo(order, 2, min(n,m)+1) {
+            fo(i, order-1, n) {
+                fo(j, order-1, m) {
+                    ll temp = pref[i][j];
+                    if(i-order>=0) temp -= pref[i-order][j];
+                    if(j-order>=0) temp -= pref[i][j-order];
+                    if(j-order>=0 and i-order>=0) temp += pref[i-order][j-order];
+                    // cout << order << " " << i << " " << j << " " << temp << endl;
+                    if(temp >= k*order*order) ans++;
+                }
+            }
         }
         cout << ans << endl;
     }
